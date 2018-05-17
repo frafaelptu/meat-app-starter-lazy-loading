025badcba92a21bd7e2f3c59c81cdaa473b0a098
frafaelptu <frafaelptu@yahoo.com.br>
2018-05-16 22:27:15 -0300

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { Component } from '@angular/core';
@@ -20,6 +19,6 @@ export const ROUTES: Routes = [
         ]},
     {path: 'order', component: OrderComponent},
     {path: 'order-summary', component: OrderSummaryComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', loadChildren: './about/about.module#AboutModule'}
 ];
 
